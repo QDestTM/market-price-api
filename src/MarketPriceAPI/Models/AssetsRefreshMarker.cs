da588db8e81eb034c32ebb55805c460382a2b904
QDestTM <dimag4681@gmail.com>
2026-03-28 09:26:32 +0200

    feat(models): create AssetsRefreshMarker model

---
+++ src/MarketPriceAPI/Models/AssetsRefreshMarker.cs
@@ -0,0 +1,23 @@
+namespace MarketPriceAPI.Models;
+
+// Namespaces used by this file
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+
+// Main content of the file
+public sealed class AssetsRefreshMarker
+{
+	public const string DefaultId = "assets_refresh_marker";
+
+	// ^ ----------------------------------------------------------------------------------------------------<
+
+	[BsonElement("_id")]
+	public string Id { get; set; } = DefaultId;
+
+	[BsonElement("refresh_at")]
+	[BsonDateTimeOptions(Kind=DateTimeKind.Utc)]
+	public DateTime RefreshAt { get; set; } = DateTime.UtcNow;
+
+	// ------------------------------------------------------------------------------------------------------<
+}
