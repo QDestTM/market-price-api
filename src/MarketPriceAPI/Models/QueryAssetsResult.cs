78e538a6ff10309abfdd6d2e0454182a615cdd7b
QDestTM <dimag4681@gmail.com>
2026-03-28 21:14:36 +0200

    feat(models): create QuertAssetsResult model

---
+++ src/MarketPriceAPI/Models/QueryAssetsResult.cs
@@ -0,0 +1,15 @@
+namespace MarketPriceAPI.Models;
+
+// Namespaces used by this file
+using System.Collections.Generic;
+
+// Main content of the file
+public sealed class QueryAssetsResult
+{
+	// ^ ----------------------------------------------------------------------------------------------------<
+
+	public IEnumerable<MarketAsset> Items { get; set; } = [];
+	public PagingInfo Paging { get; set; } = new();
+
+	// ------------------------------------------------------------------------------------------------------<
+};
